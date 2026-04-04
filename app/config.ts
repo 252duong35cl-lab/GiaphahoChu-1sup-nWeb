475e441e4df2c76b94e49a0a3da2388a9d852bb3
252duong35cl-lab <252duong35cl@gmail.com>
2026-04-04 16:49:42 +0700

    Initial upload to new repository

---
+++ app/config.ts
@@ -0,0 +1,11 @@
+const config = {
+  siteName:
+    process.env.SITE_NAME ||
+    `Gia phả họ CHU`,
+  exampleEmail: process.env.EXAMPLE_EMAIL || "",
+  examplePassword: process.env.EXAMPLE_PASSWORD || "",
+  demoDomain:
+    process.env.DEMO_DOMAIN || "https://giaphahochu.vercel.app/dashboard",
+};
+
+export default config;
